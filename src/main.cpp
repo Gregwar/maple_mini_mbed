05541467666ca116fe407c39491f3bbdba19d7f2
Gregwar <g.passault@gmail.com>
2020-12-13 16:58:55 +0100

    Main example

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
 #include <mbed.h>
 #include <USBSerial.h>
-#include "shell.h"
 
-DigitalOut LED(PB_1);
-DigitalOut DISC(PB_9);
-
-SHELL_PARAMETER_INT(i, "Test", 0);
+DigitalOut led(PB_1);
+DigitalOut usbDisconnect(PB_9);
 
 int main()
 {
-    shell_init_usb();
-    DISC = 0;
+    USBSerial usbSerial;
+    usbDisconnect = 0;
 
     while (true) {
-        LED = 0;
+        usbSerial.printf("hello\r\n");
+        led = 0;
         ThisThread::sleep_for(500);
-        LED = 1;
+        led = 1;
         ThisThread::sleep_for(500);
     }
 }
