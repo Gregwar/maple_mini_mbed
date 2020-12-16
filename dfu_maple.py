e7551682fa5866620da000e7734e5499fd3de092
Gregwar <g.passault@gmail.com>
2020-12-16 19:22:38 +0100

    Simple

--- dfu_maple.py
+++ dfu_maple.py
@@ -2,11 +2,6 @@ import sys
 import os
 Import("env")
 
-uploader = '$UPLOADER'
-
-if sys.platform != 'win32' and os.path.exists('dfu-util'):
-    uploader = './dfu-util'
-
 # DFU util to program the Maple Mini
 env.Replace(
     UPLOADER='dfu-util',
